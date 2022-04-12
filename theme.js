4e1e57ae4c7f3d5815fe7f200539b79180dd10b4
joelbarrette <joel.barrette@gmail.com>
2022-04-12 14:19:22 -0700

    json load refactor + pois

---
+++ theme.js
@@ -0,0 +1,76 @@
+var routeColor = '#000000'
+var roadColor = '#4AA96C'
+var connectorColor = '#FFE162'
+var gravelColor = '#009DAE'
+var reconColor = '#9D84B7'
+var cautionColor = '#D9534F'
+
+
+var campIcon = L.icon({
+  iconUrl: 'resources/camp.png',
+  iconSize: [23, 32],
+  iconAnchor: [11, 32],
+  popupAnchor: [0,-32],
+});
+
+var recIcon = L.icon({
+  iconUrl: 'resources/rec-site.png',
+  iconSize: [23, 32],
+  iconAnchor: [11, 32],
+  popupAnchor: [0,-32],
+});
+
+var foodIcon = L.icon({
+  iconUrl: 'resources/food.png',
+  iconSize: [23, 32],
+  iconAnchor: [11, 32],
+  popupAnchor: [0,-32],
+});
+
+var groceryIcon = L.icon({
+  iconUrl: 'resources/grocery.png',
+  iconSize: [23, 32],
+  iconAnchor: [11, 32],
+  popupAnchor: [0,-32],
+});
+
+var bikeIcon = L.icon({
+  iconUrl: 'resources/bike.png',
+  iconSize: [23, 32],
+  iconAnchor: [11, 32],
+  popupAnchor: [0,-32],
+});
+
+var cafeIcon = L.icon({
+    iconUrl: 'resources/cafe.png',
+    iconSize: [23, 32],
+    iconAnchor: [11, 32],
+    popupAnchor: [0,-32],
+});
+
+function themeMarker(icon, marker){
+    switch (icon){
+        case "tent":
+            marker.setIcon(campIcon)
+            break;
+        case "grocery":
+            marker.setIcon(groceryIcon)
+            break;
+        case "bench":
+            marker.setIcon(recIcon)
+            break;
+        case "food":
+            marker.setIcon(foodIcon)
+            break;
+        case "restaurant":
+            marker.setIcon(foodIcon)
+            break;
+        case "cafe":
+            marker.setIcon(cafeIcon)
+            break;
+        case "bikeshop":
+            marker.setIcon(bikeIcon)
+            break;       
+    }
+
+}
