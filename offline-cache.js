f57e0fba0224508aa7ad066ede954d03b2ed5c1a
joelbarrette <joel.barrette@gmail.com>
2022-05-31 18:13:12 -0700

    Update offline-cache.js

--- offline-cache.js
+++ offline-cache.js
@@ -44,51 +44,7 @@ self.addEventListener('fetch', (e) => {
   })());
 });
 
-/*self.addEventListener("fetch", (event) => {
-  // We only want to call event.respondWith() if this is a navigation request
-  // for an HTML page.
- 
-  if (event.request.url == "https://tripreport.tk" || event.request.url == "https://tripreport.tk/*" ) {
-  console.log("Checking Cache for: " + event.request.url);
-    event.respondWith(
-      (async () => {
-          console.log("Fetching Online Copy");
 
-          try{// Always try the network first.
-          const networkResponse = await fetch(event.request);
-          console.log("Received Status Code: " + networkResponse.status);
-          if (networkResponse.status == 200) {
-            console.log("Working Online");
-            const cache = await caches.open(CACHE_NAME);
-            await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
-            return networkResponse;
-          }else{
-            console.log("Fetch failed; returning offline page instead.");
-            const cache = await caches.open(CACHE_NAME);
-            const cachedResponse = await cache.match(OFFLINE_URL);
-            const responseText = await cachedResponse.text();
-            console.log(responseText);
-            const newText = responseText.replace(/<title>JB <\/title>/gi, '<title>OFFLINE </title>');
-            return new Response(newText, cachedResponse);
-          }
-        
-        } catch{ 
-            console.log("Fetch failed; returning offline page instead.");
-
-            const cache = await caches.open(CACHE_NAME);
-            const cachedResponse = await cache.match(OFFLINE_URL);
-            const responseText = await cachedResponse.text();
-            console.log(responseText);
-            const newText = responseText.replace(/<title>JB <\/title>/gi, '<title>OFFLINE </title>');
-            return new Response(newText, cachedResponse);
-          };
-         
-      })()
-    );
- } else {
-    console.log("Bypassing Cache for: " + event.request.url);
-
- }
   //}
 
   // If our if() condition is false, then this fetch handler won't intercept the
@@ -97,4 +53,4 @@ self.addEventListener('fetch', (e) => {
   // event.respondWith(), the request will be handled by the browser as if there
   // were no service worker involvement. sdfsdf
 
-});*/
+
